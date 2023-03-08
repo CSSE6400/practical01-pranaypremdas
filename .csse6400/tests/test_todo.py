8a033abf846206c544de9706548874f6ecfc7149
pranaypremdas <pranaypremdas@yahoo.com.au>
2023-03-08 22:19:01 +1000

    test uhhhhh pt 5 :(((((((

--- .csse6400/tests/test_todo.py
+++ .csse6400/tests/test_todo.py
@@ -1,15 +1,17 @@
 import unittest
 from todo import create_app
+from flask import jsonify
 
-TEST_TODO = {
+
+TEST_TODO = jsonify({
             "id": 1,
             "title": "Watch CSSE6400 Lecture",
             "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-            "completed": "true",
+            "completed": True,
             "deadline_at": "2023-02-27T00:00:00",
             "created_at": "2023-02-20T00:00:00",
             "updated_at": "2023-02-20T00:00:00"
-        }
+        })
 
 
 class TestTodo(unittest.TestCase):
